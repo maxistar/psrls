b8eb3deba9294292efde38705907aa0cb6fa30d1
Max Starikov <mstarikov@itemis.com>
2025-04-16 12:37:25 +0200

    Revert "Merge pull request #7 from Make-Europe/master"
    This reverts commit 603542778a52d7a71673dc1cbef992417d0cb0f8, reversing
    changes made to 0cf170c19aa79918aad63c0a00b8bfa47444b67f.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,20 @@
-import { useState } from 'react';
-import './App.css';
-import StartPage from './StartPage';
+import { useState } from 'react'
+import './App.css'
+import StartPage from './StartPage'
 import GuideSelector from "./GuideSelector.tsx";
 import FeedbackPage from "./FeedbackPage";
-import { TonConnectButton } from "@tonconnect/ui-react";
+import {TonConnectButton} from "@tonconnect/ui-react";
 
 function App() {
-    const [mode, setMode] = useState('start');
-
-    const mapUrls = {
-        'guide1': 'https://projects.maxistar.me/psrls/map_moekernstrase.html',
-        'guide2': 'https://projects.maxistar.me/psrls/map_friedrischain.html',
-        'guide3': 'https://projects.maxistar.me/psrls/map_alexanderplatz.html',
-        'alteOper': 'path_to/map_alte_oper_frankfurt.html',
-        'bethmannPark': 'path_to/map_bethmann_park.html',
-        'bornheimMitte': 'path_to/map_bornheim_Mitte.html'
-    };
-
+    const [mode, setMode] = useState('start'); 
+    
     const handlePageSelect = (selectedMode: string) => {
-        if (mapUrls[selectedMode]) {
-            window.Telegram.WebApp.openLink(mapUrls[selectedMode]);
+        if (selectedMode === 'guide1') {
+            window.Telegram.WebApp.openLink('https://projects.maxistar.me/psrls/map_moekernstrase.html')
+        } else if (selectedMode === 'guide2') {
+            window.Telegram.WebApp.openLink('https://projects.maxistar.me/psrls/map_friedrischain.html')
+        } else if (selectedMode === 'guide3') {
+            window.Telegram.WebApp.openLink('https://projects.maxistar.me/psrls/map_alexanderplatz.html')
         } else {
             setMode(selectedMode);
         }
@@ -27,15 +22,15 @@ function App() {
 
     return (
         <>
-            <header style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <header style={{display: 'flex', justifyContent: 'space-between'}}>
                 <span>AR Platform</span>
                 <TonConnectButton />
             </header>
-            {mode === 'start' && <StartPage handlePageSelect={handlePageSelect} />}
-            {mode === 'select' && <GuideSelector handlePageSelect={handlePageSelect} />}
-            {mode === 'feedback' && <FeedbackPage handlePageSelect={handlePageSelect} />}
+            {mode==='start' ? (<StartPage handlePageSelect={ handlePageSelect }></StartPage>) : (<></>)}
+            {mode==='select' ? (<GuideSelector handlePageSelect={ handlePageSelect }></GuideSelector>) : (<></>)}
+            {mode==='feedback' ? (<FeedbackPage handlePageSelect={ handlePageSelect }></FeedbackPage>) : (<></>)}
         </>
-    );
+    )
 }
 
-export default App;
+export default App
